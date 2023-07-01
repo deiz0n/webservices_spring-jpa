6ecbc2aa709ae9cf0b8021796570c9d8278602f0
deiz0n <eduardomacedosilva@outlook.com>
2023-07-01 17:21:24 -0300

    Criação do projeto

---
+++ src/main/java/com/deiz0n/webservices_spring_jpa/WebservicesSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.deiz0n.webservices_spring_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicesSpringJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebservicesSpringJpaApplication.class, args);
+    }
+
+}
