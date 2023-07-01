4eea9fb8a057039811f384f5edb44164a8ebcee5
deiz0n <eduardomacedosilva@outlook.com>
2023-07-01 18:00:48 -0300

    Criação do model e repository Product

---
+++ src/main/java/com/deiz0n/webservices_spring_jpa/models/Product.java
@@ -0,0 +1,36 @@
+package com.deiz0n.webservices_spring_jpa.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product  implements Serializable {
+
+    private UUID id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imgURL;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
