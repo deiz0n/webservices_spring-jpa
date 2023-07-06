b64cc2df4e05cb787d2974c515632a4cc8acb674
deiz0n <eduardomacedosilva@outlook.com>
2023-07-06 20:17:15 -0300

    Criação do UserController

---
+++ src/main/java/com/deiz0n/webservices_spring_jpa/controllers/UserController.java
@@ -0,0 +1,61 @@
+package com.deiz0n.webservices_spring_jpa.controllers;
+
+import com.deiz0n.webservices_spring_jpa.dtos.UserDTO;
+import com.deiz0n.webservices_spring_jpa.models.Product;
+import com.deiz0n.webservices_spring_jpa.models.User;
+import com.deiz0n.webservices_spring_jpa.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.List;
+import java.util.UUID;
+
+@Transactional
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @Transactional(readOnly = true)
+    @GetMapping
+    public ResponseEntity<List<User>> findAllUsers() {
+        var users = userService.getUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @Transactional(readOnly = true)
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findUserById(@PathVariable UUID id) {
+        var user = userService.getUser(id);
+        return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> addUser(@RequestBody @Valid UserDTO userDTO) {
+        var user = new User();
+        BeanUtils.copyProperties(userDTO, user);
+        var uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(userService.addUser(user));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> remUser(@PathVariable UUID id) {
+        userService.remUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable UUID id, @RequestBody @Valid User user, UserDTO userDTO) {
+        user = userService.updateUser(id, user);
+        BeanUtils.copyProperties(userDTO, user);
+        return ResponseEntity.ok().body(user);
+    }
+
+}
--- src/main/java/com/deiz0n/webservices_spring_jpa/dtos/UserDTO.java
+++ src/main/java/com/deiz0n/webservices_spring_jpa/dtos/UserDTO.java
@@ -2,6 +2,7 @@ package com.deiz0n.webservices_spring_jpa.dtos;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record UserDTO(
@@ -9,6 +10,6 @@ public record UserDTO(
         @NotBlank @Email String email,
         @NotBlank @Size(max = 100) String address,
         @NotBlank @Size(min = 13)  String phone,
-        @NotBlank String passwrord
+        @NotNull String password
 ) {
 }
